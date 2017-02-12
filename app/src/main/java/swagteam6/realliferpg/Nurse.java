ca4a81a8a443cedf0d8d10cabf482561f6f84be6
lutfisk <lhajicheteh@cpp.edu>
2017-02-11 19:10:20 -0800

    added classes that gives set attributes

---
+++ app/src/main/java/swagteam6/realliferpg/Nurse.java
@@ -0,0 +1,12 @@
+package swagteam6.realliferpg;
+
+/**
+ * Created by fluff on 11-Feb-17.
+ */
+
+public class Nurse extends mainClasses{
+
+    public Nurse(){
+        super(2, 3, 3, 1, 2, 1);
+    }
+}
